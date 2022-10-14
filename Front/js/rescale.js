6599c0bfa2396672c4534fec83cda77e09ca42c7
felixyangyicheng <felix.yang.yicheng@gmail.com>
2022-10-14 12:22:53 +0200

    regions

--- Front/js/rescale.js
+++ Front/js/rescale.js
@@ -1,21 +1,29 @@
-let dataArray = [{ value: 0.5, logDate: new Date(Date.now() - 1000) }, { value: 0.6, logDate: new Date(Date.now()) }];
-let sensors = d3.select("#sensors_chart");
+let dataArray = [];
+//select svg element
+let sensors = d3.select("#sensors_chart"); 
+//#region x, y scales definition
 let xscale = d3.scaleTime().domain([new Date(Date.now() - 60000), Date.now()])//option + shift+ 5 pour [] sur apple
     .range([0, 500]);
 let yscale = d3.scaleLinear().domain([-5, 5]).range([200, 10]);
+//#endregion x, y scales definition
+//#region x, y scales parameters
 let xAxis = d3.axisTop(xscale).ticks(d3.timeSecond.every(5));
 let yAxis = d3.axisLeft(yscale).ticks(2);
+//#endregion x, y scales parameters
+//#region x, y scales graph append and location
 const gx = sensors.append("g").attr("transform", `translate(20,  200)`)
     .attr("class", "sensor_xAxis");
 const gy = sensors.append("g").attr("transform", "translate(20,0)")
     .attr("class", "sensor_yAxis")
+//#endregion x, y scales graph append and location
 
+//#region circles and path graph append
 const gDot = sensors.append('g').attr("class", "gDot")
 const gLine = sensors.append('g').attr("class", "gLine").append("path")
-.attr("stroke", "red").style("opacity", "0.4").style("stroke-width", 0.5).style("fill", "none")
+    .attr("stroke", "red").style("opacity", "0.4").style("stroke-width", 0.5).style("fill", "none")
+//#endregion circles and path graph append
 
-
-
+//#region update or init function
 function update(data) {
     d3.select('.sensor_xAxis')
         .transition()
@@ -26,7 +34,6 @@ function update(data) {
     let dot = gDot.selectAll("circle").data(data);
     let t = d3.transition().duration(750);
     let t2 = d3.transition().duration(50);
-  
 
     dot.exit().attr("class", "exit")
         .transition(t)
@@ -57,13 +64,13 @@ function update(data) {
             dataArray.length > 100 ? color = "green" : color = "black";
             return color
         })
-  gLine.datum(data).transition(t2)
-    .attr("d", d3.line()
-          .x(d=> xscale(+d.logDate))
-          .y(d=> yscale(+d.value))
-         )
-    
+    gLine.datum(data).transition(t2)
+        .attr("d", d3.line()
+            .x(d => xscale(+d.logDate))
+            .y(d => yscale(+d.value))
+        )
 }
+//#endregion update or init function
 
 update(dataArray);
 
@@ -81,7 +88,4 @@ setInterval(function () {
     let data = { value: Math.random() * 100, logDate: new Date(Date.now()) };
     dataArray.push(data);
     updateScaleDomain(dataArray);
-
-
-
-}, 5000);//run this thang every 2 seconds
+}, 5000);//run this thang every 5 seconds
