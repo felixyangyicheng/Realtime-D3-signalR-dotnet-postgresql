9ea85229b3da1c6a2ffce5a14ad50539834493cd
unknown <felixyangyicheng@gmail.com>
2025-07-02 20:11:51 +0200

    update new version UI

--- WASM_Reatime_D3/Program.cs
+++ WASM_Reatime_D3/Program.cs
@@ -3,13 +3,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WASM_Reatime_D3.Contracts;
 using WASM_Reatime_D3.Services;
 using WASM_Reatime_D3;
-
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080/api/Tbllog/") });
 builder.Services.AddScoped<ILogRepository, LogRepository>();
-
+builder.Services.AddPWAUpdater();
 
 await builder.Build().RunAsync();
