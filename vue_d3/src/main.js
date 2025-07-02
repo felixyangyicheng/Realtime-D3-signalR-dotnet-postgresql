69ee6969d493fb3c4b264bdfb4883a3cc8533e11
unknown <felixyangyicheng@gmail.com>
2025-07-02 09:56:53 +0200

    .net10

--- vue_d3/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import signalr from "./utils/signalR";
-
-createApp(App).use(router).mount("#app");
-createApp(App).use(signalr).mount("#app");
-
