a5480b18b43f18d1fdfcd29c2f6fee912cd7cb7d
unknown <felixyangyicheng@gmail.com>
2025-07-02 11:15:43 +0200

    docker compsoe

--- Realtime_D3_WASM/Models/tbllogInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Realtime_D3_WASM.Models
-{
-    public class tbllogInfo
-    {
-        public string table { get; set; }
-        public string action { get; set; }
-        public tbllog data { get; set; }
-    }
-}
